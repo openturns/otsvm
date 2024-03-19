63f79d363c7501d480c741f71ffcb155a477315c
Julien Schueller <schueller@phimeca.com>
2024-03-19 17:11:02 +0100

    Add coverage report

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -3,21 +3,23 @@
 set -xe
 
 cd /tmp
-mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Werror -D_GLIBCXX_ASSERTIONS" \
+      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Werror -D_GLIBCXX_ASSERTIONS -fno-inline --coverage" \
       -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter" \
       -DSPHINX_FLAGS="-W -T -j4" \
-      /io
+      -B build /io
+cd build
 make install
 make tests
 ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
+lcov --no-external --capture --initial --directory $PWD --output-file lcov.info
 
 uid=$1
 gid=$2
 if test -n "${uid}" -a -n "${gid}"
 then
-  sudo cp -r ~/.local/share/doc/*/html /io
-  sudo chown -R ${uid}:${gid} /io/html
+  sudo cp -r ~/.local/share/doc/*/html lcov.info /io
+  sudo chown -R ${uid}:${gid} /io/html /io/lcov.info
 fi
